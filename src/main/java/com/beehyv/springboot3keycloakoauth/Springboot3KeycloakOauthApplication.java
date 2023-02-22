bbb137ae695096c4e433af6cfa4e7c64b90633a2
Prasanna Raghu <prasanna@beehyv.com>
2023-02-22 12:03:20 +0530

    First Commit

---
+++ src/main/java/com/beehyv/springboot3keycloakoauth/Springboot3KeycloakOauthApplication.java
@@ -0,0 +1,13 @@
+package com.beehyv.springboot3keycloakoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3KeycloakOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3KeycloakOauthApplication.class, args);
+	}
+
+}
